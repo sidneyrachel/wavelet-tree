d1b72b4e989759ad4aa439e6c782c7c6f081d046
Rachel Sidney Devianti <sidney.rachel@yahoo.co.id>
2022-05-14 07:50:49 +0900

    Refactor

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ def main():
     wavelet_tree.print_tree(is_bit=True)
     print()
 
-    print('max_elem(5,8):', wavelet_tree.max_elem(5, 8))
-    print('min_elem(5,8):', wavelet_tree.min_elem(5, 8))
-    print('range_int(5,7,9,12)', wavelet_tree.range_int(5, 7, 9, 12))
-    print(wavelet_tree.max_range(4, 9, '4', '9'))
+    print('max_elem(4,8):', wavelet_tree.max_elem(sp=4, ep=8))
+    print('min_elem(4,8):', wavelet_tree.min_elem(sp=4, ep=8))
+    print('range_int(5,7,9,12):', wavelet_tree.range_int(sp1=5, ep1=7, sp2=9, ep2=12))
+    print('max_range(4,9,4,9):', wavelet_tree.max_range(sp=4, ep=9, l='4', h='9'))
 
 
 if __name__ == '__main__':
