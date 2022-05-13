39ce88aa564867668b058e614ae2de3733bbb53b
Rachel Sidney Devianti <sidney.rachel@yahoo.co.id>
2022-05-13 18:05:35 +0900

    Refactor

--- models/wavelet_tree.py
+++ models/wavelet_tree.py
@@ -46,24 +46,33 @@ class WaveletTree(object):
 
         self.__print_tree_util(current_node, 0, bit)
 
+    def is_contain_bit(self, curr_node, sp, ep, bit):
+        rank_sp = (0 if sp == 1 else curr_node.get_rank_bit(sp - 1, bit)) + 1
+        select_sp = curr_node.get_select_bit(rank_sp, bit)
+
+        is_contain = sp <= select_sp <= ep
+
+        if is_contain:
+            select_ep = curr_node.get_select_bit(curr_node.get_rank_bit(ep, bit), bit)
+
+            return is_contain, select_sp, select_ep
+        else:
+            return is_contain, None, None
+
     def max_elem_util(self, curr_node, sp, ep):
         if len(curr_node.children) == 0:
             return curr_node.full_data[0]
 
-        rank_sp_1 = (0 if sp == 1 else curr_node.get_rank_bit(sp - 1, True)) + 1
-        select_sp_1 = curr_node.get_select_bit(rank_sp_1, True)
+        is_contain_1, select_sp_1, select_ep_1 = self.is_contain_bit(curr_node, sp, ep, True)
 
-        if sp <= select_sp_1 <= ep:  # contains 1
-            select_ep_1 = curr_node.get_select_bit(curr_node.get_rank_bit(ep, True), True)
+        if is_contain_1:
             new_sp = curr_node.get_rank_bit(select_sp_1, True)
             new_ep = curr_node.get_rank_bit(select_ep_1, True)
             curr_node = curr_node.children[1]
 
             return self.max_elem_util(curr_node, new_sp, new_ep)
         else:
-            rank_sp_0 = (0 if sp == 1 else curr_node.get_rank_bit(sp - 1, False)) + 1
-            select_sp_0 = curr_node.get_select_bit(rank_sp_0, False)
-            select_ep_0 = curr_node.get_select_bit(curr_node.get_rank_bit(ep, False), False)
+            is_contain_0, select_sp_0, select_ep_0 = self.is_contain_bit(curr_node, sp, ep, False)
             new_sp = curr_node.get_rank_bit(select_sp_0, False)
             new_ep = curr_node.get_rank_bit(select_ep_0, False)
             curr_node = curr_node.children[0]
@@ -77,20 +86,16 @@ class WaveletTree(object):
         if len(curr_node.children) == 0:
             return curr_node.full_data[0]
 
-        rank_sp_0 = (0 if sp == 1 else curr_node.get_rank_bit(sp - 1, False)) + 1
-        select_sp_0 = curr_node.get_select_bit(rank_sp_0, False)
+        is_contain_0, select_sp_0, select_ep_0 = self.is_contain_bit(curr_node, sp, ep, False)
 
-        if sp <= select_sp_0 <= ep:  # contains 0
-            select_ep_0 = curr_node.get_select_bit(curr_node.get_rank_bit(ep, False), False)
+        if is_contain_0:
             new_sp = curr_node.get_rank_bit(select_sp_0, False)
             new_ep = curr_node.get_rank_bit(select_ep_0, False)
             curr_node = curr_node.children[0]
 
             return self.min_elem_util(curr_node, new_sp, new_ep)
         else:
-            rank_sp_1 = (0 if sp == 1 else curr_node.get_rank_bit(sp - 1, True)) + 1
-            select_sp_1 = curr_node.get_select_bit(rank_sp_1, True)
-            select_ep_1 = curr_node.get_select_bit(curr_node.get_rank_bit(ep, True), True)
+            is_contain_1, select_sp_1, select_ep_1 = self.is_contain_bit(curr_node, sp, ep, True)
             new_sp = curr_node.get_rank_bit(select_sp_1, True)
             new_ep = curr_node.get_rank_bit(select_ep_1, True)
             curr_node = curr_node.children[1]
@@ -99,3 +104,16 @@ class WaveletTree(object):
 
     def min_elem(self, sp, ep):
         return self.min_elem_util(self.__root, sp, ep)
+
+    def range_int(self, sp1, ep1, sp2, ep2):
+        curr_node = self.__root
+
+        rank_sp1_0 = (0 if sp1 == 1 else curr_node.get_rank_bit(sp1 - 1, False)) + 1
+        select_sp1_0 = curr_node.get_select_bit(rank_sp1_0, False)
+
+        rank_sp1_1 = (0 if sp1 == 1 else curr_node.get_rank_bit(sp1 - 1, True)) + 1
+        select_sp1_1 = curr_node.get_select_bit(rank_sp1_1, True)
+
+        interval1_contains_0 = sp1 <= select_sp1_0 <= ep1
+        interval1_contains_1 = sp1 <= select_sp1_1 <= ep1
+
